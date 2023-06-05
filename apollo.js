4cc3575c7b782819dccfc5c14c1d07f6f6409f6f
Jezzatator <jeremie.patot@protonmail.ch>
2023-06-05 15:55:45 +0200

    fix i -> compteur

--- apollo.js
+++ apollo.js
@@ -1,8 +1,8 @@
-for (let compteur = 12; i >= 0; compteur--) {
+for (let compteur = 12; compteur >= 0; compteur--) {
      if (compteur == 8) {
           console.log("Ignition sequence start")
      } else if (compteur !== 7)
-          console.log(i)
+          console.log(compteur)
 }
 console.log("All engines running")
 console.log("Liftoff! 🚀")
