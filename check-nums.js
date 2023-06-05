f2b7fd8811836739cf54bc6490059d5248f3742d
Jezzatator <jeremie.patot@protonmail.ch>
2023-06-05 14:59:45 +0200

    Exo 3 finis

---
+++ check-nums.js
@@ -0,0 +1,12 @@
+let num1 = 28
+let num2 = 28
+
+if (num1 > num2) {
+    console.log("num1 est supérieur à num2")
+} else if (num2 > num1) {
+    console.log("num2 est supérieur à num2")
+} else if (num1 == num2) {
+    console.log("num1 et num2 sont égaux")
+} else {
+    console.log("Il y a une erreure")
+}
