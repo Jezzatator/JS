feb1778511c7f2f135c931860f6108aafb3ad31b
Jezzatator <jeremie.patot@protonmail.ch>
2023-06-05 14:46:19 +0200

    Excercie 1 finis

---
+++ apollo.js
@@ -0,0 +1,8 @@
+for (let i = 12; i >= 0; i--) {
+     if (i == 8) {
+          console.log("Ignition sequence start")
+     } else if (i !== 7)
+          console.log(i)
+}
+console.log("All engines running")
+console.log("Liftoff! 🚀")
