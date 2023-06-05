f29470b8a4ac40fba6195a38638f034d71d21bbb
Jezzatator <jeremie.patot@protonmail.ch>
2023-06-05 16:01:24 +0200

    exo finis

---
+++ swap-variables.js
@@ -0,0 +1,9 @@
+let var1 = 10;
+let var2 = 20;
+// Début de votre code
+
+[var1, var2] = [var2, var1]
+
+// Fin de votre code
+console.log(var1); // Doit afficher 20
+console.log(var2); // Doit afficher 10
